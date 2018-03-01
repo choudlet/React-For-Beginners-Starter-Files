8e6828d6fa1f1a2c5c7a765d7a6614302a92ce7d
Christopher Houdlette <christopher@choozle.com>
2018-02-28 21:43:22 -0700

    working on getting this complete

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class StorePicker extends Component {
+
+    render() {
+        return (
+            <p>Hello</p>
+        );
+    }
+}
+
+export default StorePicker;
