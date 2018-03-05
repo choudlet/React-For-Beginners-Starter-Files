beaab84fce154cbdd1c36d824d9614ef182d070a
Christopher Houdlette <christopher@choozle.com>
2018-03-04 20:20:40 -0700

    working on store picker

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,12 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class StorePicker extends Component {
-
-    render() {
-        return (
-            <p>Hello</p>
-        );
-    }
+  render() {
+    return (
+      <form className="store-selector">
+        <h2>Please Enter a Store</h2>
+        <p />
+        <input type="text" required placeholder="Store Name" />
+        <button type="submit">Visit Store</button>
+      </form>
+    );
+  }
 }
 
 export default StorePicker;
